d6bdcf9c146373c9bedd1729ab4992877f1a10e4
jkelly8823 <jkelly8823@gmail.com>
2025-05-01 17:30:44 -0500

    why no list db

--- kc_5330/welcome.py
+++ kc_5330/welcome.py
@@ -8,6 +8,8 @@ import click
 bp = Blueprint('welcome', __name__)
 
 from kc_5330.db import get_db
+
+@click.command('list-db')
 def list_db():
     db = get_db()
     tables = db.execute("SELECT name FROM sqlite_master WHERE type='table';").fetchall()
